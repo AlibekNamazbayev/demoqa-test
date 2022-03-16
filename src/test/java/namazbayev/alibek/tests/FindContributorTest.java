8b7c2aa301287fa2571da3d82fb9a1c5afc6336e
Alibek Namazbayev <namazbayev.alibek@gmail.com>
2022-03-16 23:51:55 +0600

    Added move the mouse to contributor and checking the popup

--- src/test/java/namazbayev/alibek/tests/FindContributorTest.java
+++ src/test/java/namazbayev/alibek/tests/FindContributorTest.java
@@ -1,17 +1,27 @@
 package namazbayev.alibek.tests;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import java.nio.channels.Selector;
 
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static java.lang.Thread.sleep;
 
 public class FindContributorTest {
     @Test
-    void ShouldFindContributor() {
+    void ShouldFindContributor() throws InterruptedException {
         open("https://github.com/selenide/selenide");
-        $(".layout-sidebar").$(byText("Contributors"));
+
+        $(".Layout-sidebar").$(byText("Contributors")).closest("div")
+                .$$("ul li").first().hover();
+
+        sleep(5000);
+
+        $$(".Popover-message").find(Condition.visible)
+                .shouldHave(Condition.text("Andrei Solntsev"));
+
+
     }
 }
