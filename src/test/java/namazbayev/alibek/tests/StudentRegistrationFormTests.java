e2651e5059a72221d34b27e7a1e22c16e2d5e846
alibeknamazbayev <namazbayev.alibek@gmail.com>
2022-05-30 23:50:33 +0600

    added faker

--- src/test/java/namazbayev/alibek/tests/StudentRegistrationFormTests.java
+++ src/test/java/namazbayev/alibek/tests/StudentRegistrationFormTests.java
@@ -8,9 +8,11 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static namazbayev.alibek.tests.TestDate.*;
+import static utils.RandomUtils.getRandomString;
 
 public class StudentRegistrationFormTests {
     RegistrationPage registrationPage = new RegistrationPage();
+    String firstName = getRandomString(12);
 
     @BeforeAll
     static void setUp() {
@@ -22,7 +24,7 @@ public class StudentRegistrationFormTests {
     void fillFormTest() {
         registrationPage.openPage();
 
-        registrationPage.typeFirstName(userName);
+        registrationPage.typeFirstName(firstName);
         registrationPage.typeLastName(userSurname);
         $("#userEmail").setValue(userEmail);
         $("#genterWrapper").$(byText("Other")).click();
@@ -30,9 +32,9 @@ public class StudentRegistrationFormTests {
         registrationPage.calendarComponent.setDate("30", "July", "2008 ");
         $("#subjectsInput").setValue("Math").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
-        //$("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#uploadPicture").uploadFromClasspath("img/11405.jpg");
         $("#currentAddress").setValue("Some address 1");
-        //$("#state").scrollTo().click();
+        $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Noida")).click();
@@ -40,6 +42,6 @@ public class StudentRegistrationFormTests {
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").$(byText("Student Name"))
-                .parent().shouldHave(text("Alex Egorov"));
+                .parent().shouldHave(text(firstName + " " + userName));
     }
 }
